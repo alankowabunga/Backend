0ac286f825ea043d47507c7bbd34172b8f6d21ce
Alan <chenyujen0218@gmail.com>
2023-06-28 15:38:17 +0800

    course end

---
+++ SpringBoot_Beginner/src/test/java/com/YoutubeCourse/SpringBoot_Beginner/SpringBootBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YoutubeCourse.SpringBoot_Beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
