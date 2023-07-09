574221d9a3fd3123b756ad79bea9a230b7e74ef0
Alan <babyalan0857@gmail.com>
2023-07-09 23:50:51 +0800

    home

---
+++ SpringBoot_folder/JWT_Course/JWT_Course/src/main/java/com/security/JWT_Course/JwtCourseApplication.java
@@ -0,0 +1,13 @@
+package com.security.JWT_Course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtCourseApplication.class, args);
+	}
+
+}
